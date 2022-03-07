d41f2ccad2ad1b9643aa805818bb654cec1a9d57
shakoor123 <shakoorchachu2001@gmail.com>
2022-03-07 09:42:05 +0530

    added some mysql routes

--- routes/index.js
+++ routes/index.js
@@ -5,30 +5,89 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'mysql',
-  database: 'placement'
+  password: 'Shakoor@2786',
+  database: 'placement1'
 })
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err
   console.log('You are now connected...')
+
 })
 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
   res.render('index');
 });
-router.post('/signup',(req,res)=>{
+
+//signup page
+router.get('/signup', function (req, res, next) {
+  res.render('signup');
+});
+// put into database
+router.post('/signup', (req, res) => {
   console.log(req.body);
-  console.log(req.body.id);
 
   var sql = `INSERT INTO student (id,name,branch) VALUES ("${req.body.id}","${req.body.name}","${req.body.branch}")`;
   connection.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   })
-  
+
   res.render('login')
 })
+//select into database
+
+router.get('/select', (req, res) => {
+  connection.query("SELECT * FROM student", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.render('select', { result });
+
+  });
+
+
+})
+
+
+//login operation
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+
+router.post('/login', (req, res) => {
+  console.log(req.body);
+
+  connection.query(`SELECT name FROM student where id="${req.body.id}"`, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result[0].name);
+    console.log(req.body.name);
+    if(result[0].name==req.body.name){
+      res.render('index')
+    }else{
+      res.send("invalid userid or name")
+    }
+
+  });
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
